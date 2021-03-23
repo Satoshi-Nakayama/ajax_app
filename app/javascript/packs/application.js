495143c53e15a99afe2ccf4d97bbc35b1792cd83
Satoshi-Nakayama <s91.satoshi.nakayama@gmail.com>
2021-03-23 20:57:53 +0900

    Basic検証を導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 require("../checked")
 require("../memo")
+require("../hover")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
